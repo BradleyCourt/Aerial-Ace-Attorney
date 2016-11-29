1284e565a4e7e73e7917090cf151154c9f66abd5
AIE\bradley.court <bradleycourt@hotmail.com>
2016-11-29 10:45:40 +1030

    copied everything from old project cause im an idiot and worked from there, added movement, and an animator script, pressing F flips the plane

---
+++ Assets/Scripts/animator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class animator : MonoBehaviour
+    {
+    Animator animate;
+	// Use this for initialization
+	void Start ()
+    {
+        animate = GetComponent<Animator>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetKeyDown (KeyCode.F))
+        {
+            animate.SetTrigger("startFlip");
+        }
+    }
+}
--- Assets/Scripts/red_plane_controller.cs
+++ Assets/Scripts/red_plane_controller.cs
@@ -1,15 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class red_plane_controller : MonoBehaviour {
+public class red_plane_controller : MonoBehaviour
+{
+    public int speed = 5;
+    
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition += new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0) * speed * Time.deltaTime;
+    }
 }
+
