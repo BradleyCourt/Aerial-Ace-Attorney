fe6e13b27588a86b5bedcfc5536cb5f54c189d1a
AIE\bradley.court <bradleycourt@hotmail.com>
2016-11-29 12:21:46 +1030

    Added boundaries for the player

---
+++ Assets/Scripts/cameraBounds.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraBounds : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+        
+        pos.x = Mathf.Clamp(pos.x, 0.03f, 0.95f);
+
+        pos.y = Mathf.Clamp(pos.y, 0.03f, 0.95f);
+
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
+    }
+}
