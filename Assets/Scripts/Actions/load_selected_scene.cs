73bf54c8345e9a63b51bee41fbeb71c39ceb22e7
Bradley Home <bradleycourt@hotmail.com>
2017-05-17 21:19:31 +0930

    menu's and stuff

---
+++ Assets/Scripts/Actions/load_selected_scene.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class load_selected_scene : action
+{
+    public string scenename = ""; 
+
+    // Use this for initialization
+    void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+    public override void execute()
+    {
+        SceneManager.LoadScene(scenename);
+    }
+}
+
---
+++ Assets/Scripts/action.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class action : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public virtual void execute()
+    {
+
+    }
+}
